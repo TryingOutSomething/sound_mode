7ecac7a32b2e10eba8750ce56ee84423d25d3a44
Muhammad Haris <134225645+Muhammad-Haris-2@users.noreply.github.com>
2024-12-23 09:15:22 -0800

    Update IntentManagerService.java

--- android/src/main/java/com/tryingoutsomething/soundmode/sound_mode/services/IntentManagerService.java
+++ android/src/main/java/com/tryingoutsomething/soundmode/sound_mode/services/IntentManagerService.java
@@ -2,8 +2,10 @@ package com.tryingoutsomething.soundmode.sound_mode.services;
 
 import android.content.Context;
 
+/**
+ * Interface for managing system intents.
+ */
 public interface IntentManagerService {
-
     boolean permissionsGranted();
     void launchSettings(Context context);
 }
