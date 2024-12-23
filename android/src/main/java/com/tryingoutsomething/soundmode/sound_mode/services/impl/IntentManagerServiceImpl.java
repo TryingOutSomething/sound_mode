58aac351e4f7cf9259265f194d05a61321551534
Muhammad Haris <134225645+Muhammad-Haris-2@users.noreply.github.com>
2024-12-23 09:11:05 -0800

    Update IntentManagerServiceImpl.java

--- android/src/main/java/com/tryingoutsomething/soundmode/sound_mode/services/impl/IntentManagerServiceImpl.java
+++ android/src/main/java/com/tryingoutsomething/soundmode/sound_mode/services/impl/IntentManagerServiceImpl.java
@@ -6,36 +6,25 @@ import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
 
-import androidx.annotation.RequiresApi;
-
 import com.tryingoutsomething.soundmode.sound_mode.services.IntentManagerService;
 
 public class IntentManagerServiceImpl implements IntentManagerService {
 
-    NotificationManager notificationManager;
+    private final NotificationManager notificationManager;
 
-    public IntentManagerServiceImpl(NotificationManager nm) {
-        this.notificationManager = nm;
-    }
-
-    private boolean apiIsAboveMarshmallow() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    public IntentManagerServiceImpl(NotificationManager notificationManager) {
+        this.notificationManager = notificationManager;
     }
 
     @Override
     public boolean permissionsGranted() {
-        if (apiIsAboveMarshmallow()) {
-            return notificationManager.isNotificationPolicyAccessGranted();
-        }
-
-        return true;
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || notificationManager.isNotificationPolicyAccessGranted();
     }
 
     @Override
     public void launchSettings(Context context) {
         Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
         context.startActivity(intent);
     }
 }
