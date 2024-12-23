48b4e04b8b4ec9d982746855813738474a0a450f
Muhammad Haris <134225645+Muhammad-Haris-2@users.noreply.github.com>
2024-12-23 09:13:57 -0800

    Update AudioManagerServiceImpl.java

--- android/src/main/java/com/tryingoutsomething/soundmode/sound_mode/services/impl/AudioManagerServiceImpl.java
+++ android/src/main/java/com/tryingoutsomething/soundmode/sound_mode/services/impl/AudioManagerServiceImpl.java
@@ -1,35 +1,43 @@
 package com.tryingoutsomething.soundmode.sound_mode.services.impl;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.media.AudioManager;
+import android.os.Build;
 
 import com.tryingoutsomething.soundmode.sound_mode.services.AudioManagerService;
 
-/**
- * @author TryingOutSomething
- */
 public class AudioManagerServiceImpl implements AudioManagerService {
 
     private final String NORMAL_MODE_PROFILE = "normal";
     private final String VIBRATE_MODE_PROFILE = "vibrate";
     private final String SILENT_MODE_PROFILE = "silent";
+    private final String DND_MODE_PROFILE = "dnd";
 
     private final AudioManager audioManager;
+    private final NotificationManager notificationManager;
 
-    public AudioManagerServiceImpl(AudioManager am) {
-        this.audioManager = am;
+    public AudioManagerServiceImpl(AudioManager audioManager, NotificationManager notificationManager) {
+        this.audioManager = audioManager;
+        this.notificationManager = notificationManager;
     }
 
     @Override
     public String getCurrentRingerMode() {
-        int ringerMode = audioManager.getRingerMode();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int interruptionFilter = notificationManager.getCurrentInterruptionFilter();
+            if (interruptionFilter == NotificationManager.INTERRUPTION_FILTER_NONE) {
+                return DND_MODE_PROFILE;
+            }
+        }
 
-        switch (ringerMode) {
+        switch (audioManager.getRingerMode()) {
             case AudioManager.RINGER_MODE_NORMAL:
                 return NORMAL_MODE_PROFILE;
-            case AudioManager.RINGER_MODE_SILENT:
-                return SILENT_MODE_PROFILE;
             case AudioManager.RINGER_MODE_VIBRATE:
                 return VIBRATE_MODE_PROFILE;
+            case AudioManager.RINGER_MODE_SILENT:
+                return SILENT_MODE_PROFILE;
             default:
                 return null;
         }
@@ -37,21 +45,15 @@ public class AudioManagerServiceImpl implements AudioManagerService {
 
     @Override
     public String setRingerMode(int mode) {
-        switch (mode) {
-            case AudioManager.RINGER_MODE_NORMAL:
-                audioManager.setRingerMode(mode);
-                return NORMAL_MODE_PROFILE;
-
-            case AudioManager.RINGER_MODE_SILENT:
-                audioManager.setRingerMode(mode);
-                return SILENT_MODE_PROFILE;
-
-            case AudioManager.RINGER_MODE_VIBRATE:
-                audioManager.setRingerMode(mode);
-                return VIBRATE_MODE_PROFILE;
-
-            default:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && mode == AudioManager.RINGER_MODE_SILENT) {
+            if (!notificationManager.isNotificationPolicyAccessGranted()) {
                 return null;
+            }
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
+            return DND_MODE_PROFILE;
         }
+
+        audioManager.setRingerMode(mode);
+        return mode == AudioManager.RINGER_MODE_VIBRATE ? VIBRATE_MODE_PROFILE : NORMAL_MODE_PROFILE;
     }
 }
